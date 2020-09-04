e2a7e92f78a52fd4705311f7124b296c347e7acd
Sanae <sanae.kawasaka@gmail.com>
2020-09-04 10:00:17 +0900

    added phpdotenv

--- dbconnect.php
+++ dbconnect.php
@@ -1,64 +1,66 @@
 <?php
+require './vendor/autoload';
 
-    $post = $_POST;
-    $date = $post["date"];
-    $category = $post["category"];
-    $expense = $post["expense"];
+use Dotenv\Dotenv;
 
-    $host = $_ENV['DB_HOST'];
-    $dbname = $_ENV['DB_NAME'];
-    $password = $_ENV['DB_PASSWORD'];
-    $user = $_ENV['DB_USER'];
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-   
-        try{
+$post = $_POST;
+$date = $post["date"];
+$category = $post["category"];
+$expense = $post["expense"];
 
-            if ($_SERVER["REQUEST_METHOD"] !== "POST" || $post["button"] !== "click" ||
-                empty($date) || empty($category) || empty($expense)
-            ){
-                header("location: myExpenses.php");
-                exit;
-            }
+$host = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$password = $_ENV['DB_PASSWORD'];
+$user = $_ENV['DB_USER'];
 
-            // データベースに接続
-            $PDO = new PDO("mysql:dbname=$dbname;host=$host;charset=utf8", $user, $password);
-            $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // SQLでエラーが表示された場合、画面にエラーが出力される
-            $PDO->setattribute(PDO::ATTR_EMULATE_PREPARES, false); // SQLインジェクション対策
 
-            // データベースに書き込み
-            $sql = "INSERT INTO expenses (date, category, expense) VALUES (?, ?, ?)";
-            $stmt = $PDO->prepare($sql);
-            $params = [$date, $category, $expense];
-            $stmt->execute($params);
+try {
+    if (
+        $_SERVER["REQUEST_METHOD"] !== "POST" || $post["button"] !== "click" ||
+        empty($date) || empty($category) || empty($expense)
+    ) {
+        header("location: myExpenses.php");
+        exit;
+    }
 
-            // データベースのexpensesからデータを全て取得
-            $sql = "SELECT * FROM expenses WHERE expense BETWEEN 10 AND 100000";
-            $stmt = $PDO->query($sql);
+    // データベースに接続
+    $PDO = new PDO("mysql:dbname=$dbname;host=$host;charset=utf8", $user, $password);
+    $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // SQLでエラーが表示された場合、画面にエラーが出力される
+    $PDO->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); // SQLインジェクション対策
 
-            // データベースのexpenseの合計を取得
-            $sum = "SELECT SUM(expense) AS total FROM expenses";
-            $total_expenses = $PDO->query($sum);
+    // データベースに書き込み
+    $sql = "INSERT INTO expenses (date, category, expense) VALUES (?, ?, ?)";
+    $stmt = $PDO->prepare($sql);
+    $params = [$date, $category, $expense];
+    $stmt->execute($params);
 
-            // データベースのカラムに名前を付けて、合計を取得
-            $sum_food = "SELECT SUM(expense) AS total_food FROM expenses WHERE category = 'Food'";
-            $total_food = $PDO->query($sum_food);
+    // データベースのexpensesからデータを全て取得
+    $sql = "SELECT * FROM expenses WHERE expense BETWEEN 10 AND 100000";
+    $stmt = $PDO->query($sql);
 
-            $sum_snack_sweets = "SELECT SUM(expense) AS total_snack_sweets FROM expenses WHERE category = 'Snack/Sweets'";
-            $total_snack_sweets = $PDO->query($sum_snack_sweets);
+    // データベースのexpenseの合計を取得
+    $sum = "SELECT SUM(expense) AS total FROM expenses";
+    $total_expenses = $PDO->query($sum);
 
-            $sum_dine_out = "SELECT SUM(expense) AS total_dine_out FROM expenses WHERE category = 'Dine_out'";
-            $total_dine_out = $PDO->query($sum_dine_out);
+    // データベースのカラムに名前を付けて、合計を取得
+    $sum_food = "SELECT SUM(expense) AS total_food FROM expenses WHERE category = 'Food'";
+    $total_food = $PDO->query($sum_food);
 
-            $sum_living_expenses = "SELECT SUM(expense) AS total_living_expenses FROM expenses WHERE category = 'Living_expenses'";
-            $total_living_expenses = $PDO->query($sum_living_expenses);
+    $sum_snack_sweets = "SELECT SUM(expense) AS total_snack_sweets FROM expenses WHERE category = 'Snack/Sweets'";
+    $total_snack_sweets = $PDO->query($sum_snack_sweets);
 
-            $sum_entertainment_expenses = "SELECT SUM(expense) AS total_entertainment_expenses FROM expenses WHERE category = 'Entertainment_expenses'";
-            $total_entertainment_expenses = $PDO->query($sum_entertainment_expenses);
+    $sum_dine_out = "SELECT SUM(expense) AS total_dine_out FROM expenses WHERE category = 'Dine_out'";
+    $total_dine_out = $PDO->query($sum_dine_out);
 
-        }catch(PDOException $e) {
+    $sum_living_expenses = "SELECT SUM(expense) AS total_living_expenses FROM expenses WHERE category = 'Living_expenses'";
+    $total_living_expenses = $PDO->query($sum_living_expenses);
 
-            exit("データベースに：接続出来ませんでした。".$e->getMessage());
+    $sum_entertainment_expenses = "SELECT SUM(expense) AS total_entertainment_expenses FROM expenses WHERE category = 'Entertainment_expenses'";
+    $total_entertainment_expenses = $PDO->query($sum_entertainment_expenses);
+} catch (PDOException $e) {
 
-        } 
-    
-    
+    exit("データベースに：接続出来ませんでした。" . $e->getMessage());
+}
--- myExpenses.php
+++ myExpenses.php
@@ -1,15 +1,16 @@
 <?php
-    require_once("class.php");
+require_once("class.php");
 
-    // class.phpからの取得要素
-    $post = $_POST;
-    $expense = new expense($post);
-    $expense->month = "March";
+// class.phpからの取得要素
+$post = $_POST;
+$expense = new expense($post);
+$expense->month = "March";
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,36 +18,38 @@
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-        <header>
-            <h1><?php echo "The Expenses of $expense->month"?></h1>
-        </header>
+    <header>
+        <h1><?php echo "The Expenses of $expense->month" ?></h1>
+    </header>
 
-        <main>
-            <form action="myExpensesResult.php" method="POST">
-                <div class="input_area">
+    <main>
+        <form action="myExpensesResult.php" method="POST">
+            <div class="input_area">
 
-                    <div class="date">
-                        Date: <input type="date" name="date" value="">
-                    </div>
-
-                    <div class="category">
-                        Category: <select name="category" id="">
-                            <?php echo $expense->OutputCategory() ?>
-                        </select>
-                    </div>
-
-                    <div class="expense">
-                        Expense: <input type="number" name="expense" value=""><br>
-                    </div>
-
+                <div class="date">
+                    Date: <input type="date" name="date" value="">
                 </div>
-                <div class="button">
-                    <button type="submit" name="button" value="click">Caluculate</button>
-                </div>
-            </form>
-        </main>
 
-        <footer></footer>
+                <div class="category">
+                    Category: <select name="category" id="">
+                        <?php echo $expense->OutputCategory() ?>
+                    </select>
+                </div>
+
+                <div class="expense">
+                    Expense: <input type="number" name="expense" value=""><br>
+                </div>
+
+            </div>
+            <div class="button">
+                <button type="submit" name="button" value="click">Caluculate</button>
+            </div>
+        </form>
+    </main>
+
+    <footer></footer>
 </body>
+
 </html>
